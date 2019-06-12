01a62ef434cefcda0529fc70d454ebabf751812c
Adrian Sims <adriansims62781@gmail.com>
2019-06-12 20:54:36 +0200

    Add project files.

---
+++ FizzOptimal/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+// c# code golf fizz buzz
+class Program
+{
+    static void Main()
+    {
+        Enumerable.Range(1, 100).ToList().ForEach(x => Console.WriteLine(new[] { $"{x}", "fizz", "buzz", "fizz buzz" }[1 & 1 << x % 3 | 2 & 2 << x % 5]));
+    }
+}
